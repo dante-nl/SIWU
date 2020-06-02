6604c273d2acf5ab3093ee76bd8c2e63603d2bba
dante-nl <61943433+dante-nl@users.noreply.github.com>
2020-06-02 16:23:11 +0200

    Delete redirect.php

--- redirect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$shortcut = $_REQUEST['shortcut'] ?: 'OAuth%202.0';
-$code = $_REQUEST['code'];
-
-header('Location: ' . 'shortcuts://run-shortcut?name=' . rawurlencode($shortcut) . '&input=text&text=' . rawurlencode($code));
-die();
-?>
